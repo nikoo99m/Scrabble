48393c5c2ac4c613de1fb2e36eb63af02cdfe2b3
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-22 13:41:29 +0000

    docs: Adding JavaDoc comments for BracketSquare class.

--- src/pij/main/models/Squares/BracketSquare.java
+++ src/pij/main/models/Squares/BracketSquare.java
@@ -2,12 +2,24 @@ package pij.main.models.Squares;
 
 import pij.main.utils.StringHelper;
 
+/**
+ * Represents a premium letter square in the game board.
+ */
 public class BracketSquare extends Square {
-
+    /**
+     * Constructor.
+     *
+     * @param mul The multiplier associated with the bracket square.
+     */
     public BracketSquare(int mul)
     {
         this.multiplier = mul;
     }
+    /**
+     * Generates the text representation of the bracket square.
+     *
+     * @return A string representing the bracket square with its multiplier.
+     */
     @Override public String generateSquareText(){
         return StringHelper.set5("(" + multiplier + ")");
     }
