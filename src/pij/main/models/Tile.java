9105cfcb26091d1829c7cdffc010d8633f0c87c2
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-22 15:00:04 +0000

    docs: Adding JavaDoc comments for Tile class.

--- src/pij/main/models/Tile.java
+++ src/pij/main/models/Tile.java
@@ -2,15 +2,34 @@ package pij.main.models;
 
 import pij.main.utils.StringHelper;
 
+/**
+ * Represents a tile with a character and a value.
+ * <p>
+ * An instance contains
+ * - character
+ * - value
+ */
 public class Tile {
     public String character;
     public int value;
-    public Tile(String character, int value)
-    {
+
+    /**
+     * Constructs a new Tile with the given character and value.
+     *
+     * @param character the character of the tile
+     * @param value     the value of the tile
+     */
+    public Tile(String character, int value) {
         this.character = character;
         this.value = value;
     }
-    public String ToString(){
+
+    /**
+     * Returns a string representation of the tile.
+     *
+     * @return a string representing the tile's character and value
+     */
+    public String ToString() {
         return StringHelper.set5(character + value);
     }
 }
