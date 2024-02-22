607670111dbbd76f4af539f5a01fec7e2ebea49f
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-22 19:39:46 +0000

    docs: Adding JavaDoc comments for StringHelper class.

--- src/pij/main/utils/StringHelper.java
+++ src/pij/main/utils/StringHelper.java
@@ -2,15 +2,21 @@ package pij.main.utils;
 
 import pij.main.models.Location;
 
+/**
+ * This class provides utility methods for manipulating strings.
+ */
 public class StringHelper {
-    public static String set5(String inputString)
-    {
+    /**
+     * Ensures that the input string has a length of at least 5 characters by adding spaces to the end if necessary.
+     *
+     * @param inputString The input string to be modified.
+     * @return The modified string with a length of at least 5 characters.
+     */
+    public static String set5(String inputString) {
         int length = 5;
         if (inputString.length() < length) {
-            // Calculate the number of spaces needed
             int spacesToAdd = length - inputString.length();
 
-            // Add spaces to the end of the string
             for (int i = 0; i < spacesToAdd; i++) {
                 inputString += " ";
             }
@@ -18,11 +24,18 @@ public class StringHelper {
         return inputString;
     }
 
-    public static String printLocation(Location location, boolean isVertical)
-    {
+    /**
+     * Prints the location string representation.
+     *
+     * @param location   The location object to be printed.
+     * @param isVertical A boolean indicating whether to print in vertical format.
+     * @return The string representation of the location.
+     */
+
+    public static String printLocation(Location location, boolean isVertical) {
         String locationString = location.toString();
-        if(isVertical)
-            return locationString.substring(locationString.length() - 1) +  locationString.substring(0, locationString.length() - 1) ;
+        if (isVertical)
+            return locationString.substring(locationString.length() - 1) + locationString.substring(0, locationString.length() - 1);
         else
             return location.toString();
     }
