300ea1080680115baea828a67755e251c638eb80
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-22 13:47:49 +0000

    docs: Adding JavaDoc comments for CurlySquare class.

--- src/pij/main/models/Squares/CurlySquare.java
+++ src/pij/main/models/Squares/CurlySquare.java
@@ -1,13 +1,23 @@
 package pij.main.models.Squares;
 
 import pij.main.utils.StringHelper;
-
+/**
+ * Represents a premium word square in the game. Extends the Square class.
+ * Each CurlySquare has a multiplier value that affects the player's score.
+ */
 public class CurlySquare extends Square {
-
+    /**
+     * Constructor.
+     * @param mul the multiplier value for this CurlySquare.
+     */
     public CurlySquare(int mul)
     {
         this.multiplier = mul;
     }
+    /**
+     * Generates the text representation of this CurlySquare.
+     * @return A string representing CurlySquare with its multiplier.
+     */
     @Override public String generateSquareText(){
         return StringHelper.set5("{" + multiplier + "}");
     }
