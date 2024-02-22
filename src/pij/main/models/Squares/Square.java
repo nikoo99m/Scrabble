4d9cee47ccc78a954abb8b080a626240483f0029
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-22 14:01:48 +0000

    docs: Adding JavaDoc comments for Square class.

--- src/pij/main/models/Squares/Square.java
+++ src/pij/main/models/Squares/Square.java
@@ -2,13 +2,33 @@ package pij.main.models.Squares;
 
 import pij.main.models.Tile;
 
+/**
+ * Represents a square on the game board.
+ * Squares can either contain a tile or be empty.
+ * Each square may also have a multiplier associated with it.
+ * <p>
+ * An instance contains
+ * multiplier
+ * tile
+ */
 public abstract class Square{
     public int multiplier;
     public Tile tile;
+    /**
+     * Sets the tile of this square to the given tile.
+     * @param givenTile the tile to be set.
+     */
     public void setTile(Tile givenTile)
     {
         this.tile = givenTile;
     }
+
+    /**
+     * Returns the string representation of this square.
+     * If the square contains a tile, returns the tile's string representation.
+     * Otherwise, generates and returns the square's text representation.
+     * @return the string representation of this square
+     */
     @Override
     public String toString(){
         if(tile == null)
@@ -16,5 +36,10 @@ public abstract class Square{
         else
             return tile.ToString();
     }
+    /**
+     * Generates the text representation of this square.
+     * Subclasses must implement this method to provide the specific format of the square's text.
+     * @return the text representation of this square
+     */
     public abstract String generateSquareText();
 }
