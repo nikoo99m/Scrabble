84f923de0163ec21b1718ecec7d4601576638d90
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-22 19:30:29 +0000

    docs: Adding JavaDoc comments for GameHelper class.

--- src/pij/main/utils/GameHelper.java
+++ src/pij/main/utils/GameHelper.java
@@ -5,14 +5,29 @@ import pij.main.models.MethodReturns.WordChoice;
 import pij.main.models.Result;
 import pij.main.services.Board;
 
+/**
+ * This class provides utility methods to assist with game logic and operations.
+ */
 public class GameHelper {
+    /**
+     * Checks if the provided location string represents a vertical move or not.
+     *
+     * @param location The location string to be checked.
+     * @return {@code true} if the location is vertical, {@code false} otherwise.
+     */
     public static boolean isVertical(String location) {
         if (Character.isDigit(location.charAt(0))) {
             return false;
         }
         return true;
     }
-
+    /**
+     * Retrieves the accepted word and its starting point based on the result of a move and the current board state.
+     *
+     * @param result The result of a move.
+     * @param board The game board.
+     * @return A WordChoice object containing the accepted word and its starting point.
+     */
     public static WordChoice getAcceptedWord(Result result, Board board) {
         StringBuilder acceptedWord = new StringBuilder();
         int n = 0;
@@ -48,11 +63,26 @@ public class GameHelper {
         }
         return new WordChoice(acceptedWord.toString(), wordStartingPoint);
     }
-
+    /**
+     * Checks if the provided coordinates are out of the game board bounds or not.
+     *
+     * @param i The row index.
+     * @param j The column index.
+     * @param board The game board.
+     * @return {@code true} if the coordinates are out of bounds, {@code false} otherwise.
+     */
     private static boolean outOfBoard(int i, int j, Board board) {
         return i >= board.getSize() || j >= board.getSize();
     }
-
+    /**
+     * Checks if the next tile in the specified direction is empty.
+     *
+     * @param isVertical {@code true} if the direction is vertical, {@code false} otherwise.
+     * @param i The current row index.
+     * @param j The current column index.
+     * @param board The game board.
+     * @return {@code true} if the next tile is empty, {@code false} otherwise.
+     */
     private static boolean nextTileIsEmpty(boolean isVertical, int i, int j, Board board) {
 
         return board.letter[i][j].tile == null;
