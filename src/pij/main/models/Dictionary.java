bfa61bb1355b83228bfdb8080743b6e89847dff3
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-22 14:22:54 +0000

    docs: Adding JavaDoc comments for Dictionary class.

--- src/pij/main/models/Dictionary.java
+++ src/pij/main/models/Dictionary.java
@@ -5,16 +5,31 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Represents a dictionary of words loaded from a file.
+ * The dictionary is used to check if a given word exists in the word list.
+ */
 public class Dictionary {
     private ArrayList<String> wordList = new ArrayList<String>();
 
-    public  Dictionary(){
+    public Dictionary() {
         load();
     }
+
+    /**
+     * Checks if a given word exists in the dictionary.
+     *
+     * @param word the word to check
+     * @return true if the word exists in the dictionary, false otherwise
+     */
     public boolean exists(String word) {
         return wordList.contains(word.toLowerCase());
     }
 
+    /**
+     * Loads the word list from a file.
+     * The file should be located at "resources\\wordlist.txt".
+     */
     public void load() {
         File file = new File("resources\\wordlist.txt");
         Scanner scanner = null;
