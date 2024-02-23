0c463dc3607a8ba2415f3dab85cc0dcf5b855e06
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-23 01:57:57 +0000

    docs: Adding JavaDoc comments for InvalidBoardSizeException class.

--- src/pij/main/exceptions/InvalidBoardSizeException.java
+++ src/pij/main/exceptions/InvalidBoardSizeException.java
@@ -1,5 +1,7 @@
 package pij.main.exceptions;
-
+/**
+ * Exception thrown when an invalid board size is provided.
+ */
 public class InvalidBoardSizeException extends Exception{
     public InvalidBoardSizeException(int size) {
         super("An invalid board size was provided: " + size);
