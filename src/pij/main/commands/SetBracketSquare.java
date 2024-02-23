a268fd8d6ab59fc45410e7e84a1e3a885a8eed44
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-23 02:40:55 +0000

    docs: Adding JavaDoc comments for SetBracketSquare class.

--- src/pij/main/commands/SetBracketSquare.java
+++ src/pij/main/commands/SetBracketSquare.java
@@ -3,7 +3,10 @@ package pij.main.commands;
 import pij.main.services.Board;
 import pij.main.models.squares.BracketSquare;
 import pij.main.models.squares.Square;
-
+/**
+ * Class representing a command to set a bracket square(premium letter square) on the game board.
+ * A bracket square is a type of premium square that provides special bonuses or penalties to players.
+ */
 public class SetBracketSquare extends PremiumSquare
 {
     private final String field;
