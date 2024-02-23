dcbf862ad8fd744ee7c9d81675c8e9f7a5f9f954
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-23 01:59:09 +0000

    docs: Adding JavaDoc comments for InvalidValueFormatForBoardSizeException class.

--- src/pij/main/exceptions/InvalidValueFormatForBoardSizeException.java
+++ src/pij/main/exceptions/InvalidValueFormatForBoardSizeException.java
@@ -1,5 +1,8 @@
 package pij.main.exceptions;
 
+/**
+ * Exception thrown when an invalid value format is supplied for the board size.
+ */
 public class InvalidValueFormatForBoardSizeException extends Exception{
     public InvalidValueFormatForBoardSizeException(String size) {
         super("Invalid value format supplied for board size: " + size);
