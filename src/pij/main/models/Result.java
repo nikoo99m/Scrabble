3ca181837f9c475460a13a2f299cb0c2788e51b4
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-23 01:14:27 +0000

    docs: Adding JavaDoc comments for Result record and CheckIsInDictionaryReturn class.

--- src/pij/main/models/Result.java
+++ src/pij/main/models/Result.java
@@ -1,4 +1,8 @@
 package pij.main.models;
-
+/**
+ * Represents the result of a player's move in the game.
+ * It includes information such as the selected tiles for the move, whether the move is vertical,
+ * and the location on the game board where the move starts.
+ */
 public record Result(String tileSelection, boolean vertical, Location location) {
 }
--- src/pij/main/models/methodReturns/CheckIsInDictionaryReturn.java
+++ src/pij/main/models/methodReturns/CheckIsInDictionaryReturn.java
@@ -1,5 +1,11 @@
 package pij.main.models.methodReturns;
-
+/**
+ * Represents the result of checking if a word is present in the dictionary.
+ * <p>
+ * An instance contains
+ * - The accepted word found in the dictionary.
+ * - Presence of the word in the dictionary.
+ */
 public class CheckIsInDictionaryReturn {
     public String acceptedWord;
     public boolean isInDictionary;
