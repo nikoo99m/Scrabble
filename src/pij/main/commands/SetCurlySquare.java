24f8e15b27ff2b2163bd94a25bafd3114d6d9c0f
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-23 02:38:38 +0000

    docs: Adding JavaDoc comments for SetCurlySquare class.

--- src/pij/main/commands/SetCurlySquare.java
+++ src/pij/main/commands/SetCurlySquare.java
@@ -3,7 +3,10 @@ package pij.main.commands;
 import pij.main.services.Board;
 import pij.main.models.squares.CurlySquare;
 import pij.main.models.squares.Square;
-
+/**
+ * Class representing a command to set a curly square(Premium word square) on the game board.
+ * A curly square is a type of premium square that provides special bonuses or penalties to players.
+ */
 public class SetCurlySquare extends PremiumSquare
 {
     private final String field;
