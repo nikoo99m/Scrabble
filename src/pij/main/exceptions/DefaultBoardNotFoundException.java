06426a5e60b57536f17d160b37563dfb35728492
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-23 01:57:33 +0000

    docs: Adding JavaDoc comments for DefaultBoardNotFoundException class.

--- src/pij/main/exceptions/DefaultBoardNotFoundException.java
+++ src/pij/main/exceptions/DefaultBoardNotFoundException.java
@@ -1,5 +1,8 @@
 package pij.main.exceptions;
-
+/**
+ * Exception thrown when there is an issue with loading the default board.
+ * This exception indicates that the default board could not be found.
+ */
 public class DefaultBoardNotFoundException extends Exception{
     public DefaultBoardNotFoundException() {
         super("There's an issue with loading the default board.");
