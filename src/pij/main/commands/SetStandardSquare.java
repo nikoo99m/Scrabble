e59dcf45bb81262f69019d0b67ee8bb938fc2134
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-23 02:38:20 +0000

    docs: Adding JavaDoc comments for SetStandardSquare class.

--- src/pij/main/commands/SetStandardSquare.java
+++ src/pij/main/commands/SetStandardSquare.java
@@ -3,7 +3,10 @@ package pij.main.commands;
 import pij.main.services.Board;
 import pij.main.models.squares.Square;
 import pij.main.models.squares.StandardSquare;
-
+/**
+ * Class representing a command to set a standard square on the game board.
+ * A standard square does not have any special attributes or bonuses.
+ */
 public class SetStandardSquare extends SetFieldSquare
 {
 
