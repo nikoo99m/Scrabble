981ef9e0cacf9245cd035c8a3898e084b359669d
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-23 01:46:15 +0000

    docs: Adding JavaDoc comments for WildCardReturn class.

--- src/pij/main/models/methodReturns/WildCardReturn.java
+++ src/pij/main/models/methodReturns/WildCardReturn.java
@@ -1,5 +1,7 @@
 package pij.main.models.methodReturns;
-
+/**
+ * Represents the result of checking for the presence of a wild card in a player's rack and clarify the index of the wildCard in player's rack.
+ */
 public class WildCardReturn {
     public int index;
     public boolean isWildCard;
