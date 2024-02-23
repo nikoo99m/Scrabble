98ade91e3de802f92eb8c72ec49650d4f1392a3a
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-23 01:56:58 +0000

    docs: Adding JavaDoc comments for BoardContentSizeMismatchException Exception class.

--- src/pij/main/exceptions/BoardContentSizeMismatchException.java
+++ src/pij/main/exceptions/BoardContentSizeMismatchException.java
@@ -1,5 +1,7 @@
 package pij.main.exceptions;
-
+/**
+ * Exception thrown when the content of the game board does not match the specified board size.
+ */
 public class BoardContentSizeMismatchException extends Exception{
     public BoardContentSizeMismatchException() {
         super("The board content does not match the specified board size.");
