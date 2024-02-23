dc1094930801a1cf4aa57a8f79d94b12c6c2ddf2
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-23 01:58:36 +0000

    docs: Adding JavaDoc comments for InvalidInputFormatAsPremiumFieldException class.

--- src/pij/main/exceptions/InvalidInputFormatAsPremiumFieldException.java
+++ src/pij/main/exceptions/InvalidInputFormatAsPremiumFieldException.java
@@ -1,5 +1,7 @@
 package pij.main.exceptions;
-
+/**
+ * Exception thrown when an invalid input format is provided for a premium board field.
+ */
 public class InvalidInputFormatAsPremiumFieldException extends Exception{
     public InvalidInputFormatAsPremiumFieldException(String value) {
         super("Invalid value format provided for premium board field: " + value);
