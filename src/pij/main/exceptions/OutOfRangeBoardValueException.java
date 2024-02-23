cc0c263c740031ef437f9a7c2d993293449f684a
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-23 01:59:27 +0000

    docs: Adding JavaDoc comments for OutOfRangeBoardValueException class.

--- src/pij/main/exceptions/OutOfRangeBoardValueException.java
+++ src/pij/main/exceptions/OutOfRangeBoardValueException.java
@@ -1,5 +1,7 @@
 package pij.main.exceptions;
-
+/**
+ * Exception thrown when a supplied value for a board field is out of range.
+ */
 public class OutOfRangeBoardValueException extends Exception{
     public OutOfRangeBoardValueException() {
         super("Supplied value for board field is out of range.");
